9690214f567c3b42e92c1791e5936d87b87bf603
sowmiya <sowmiyajhansi@gmail.com>
2021-12-01 20:20:47 +0530

    Checkpoint 11

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+if(document.getElementById('log').innerText === "Logout")
+{
+        document.getElementById('log').style.display = "hidden"; 
+        document.getElementById('log').innerText = "Login" 
+    } 
+alert("Please enter the details");
+
+    const searchBarDiv = document.getElementById("searchbar");
